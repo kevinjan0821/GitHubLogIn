bd4319ff07eebd4b2299bb94f372d0be9602c4dc
Kevin Jan Alvarez <kj.c.alvarez@gmail.com>
2021-01-07 01:51:14 +0800

    commit

--- GitHubLogIn/Shared/Services.cs
+++ GitHubLogIn/Shared/Services.cs
@@ -11,12 +11,18 @@ using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 using System.Net;
 using System.Net.Http.Formatting;
 using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace GitHubLogIn.Shared
 {
     public class Services
     {
+        private class EventModel
+        {
+            public string eventType;
+            public DateTime time; 
 
+        }
 
 
         //static HttpClient client = new HttpClient();
@@ -36,45 +42,79 @@ namespace GitHubLogIn.Shared
 
         public static async Task<string> ExecuteAsync(string userName, string password)
         {
-            string response = string.Empty;
-
+            string jsonResult = string.Empty;
+            var cookieContainer = new CookieContainer();
             var handler = new HttpClientHandler();
  
-            handler.UseDefaultCredentials = true;
+                handler.UseDefaultCredentials = true;
+                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                handler.UseCookies = true;
 
             var client = new HttpClient(handler);
-
-            client.BaseAddress = new Uri("https://api.github.com");
-
-            
-            client.DefaultRequestHeaders.Clear();
-            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Product", "1.0"));
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("CookiName", "cookine_value");
-
             var authenticationBytes = Encoding.ASCII.GetBytes(userName + ":" + password);
 
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authenticationBytes));
+            // Address
+            string address = "https://github.com/settings/security-log";
+            var uri = new Uri(address);
 
-            var result = await client.GetAsync("/users/"+ userName).ConfigureAwait(false);
-           
+            // Set headers and url details for authentication and to access the web page
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+            client.DefaultRequestHeaders.Add("Accept", "application/xhtml+xml");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authenticationBytes));
+            client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9");
+            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+
+            /* NOTE:
+            // I was able to retrieve the https://github.com/settings/security-log by mannually injecting the cookie
+            // from inspecting the web page and still looking for a way to make it dynamic*/
+            // client.DefaultRequestHeaders.Add("Cookie", "INJECT COOKIE VALUE FOR TEST");
+
+            var result =  await client.GetAsync(uri).ConfigureAwait(false);
 
             if (result.IsSuccessStatusCode)
             {
-                response = await result.Content.ReadAsStringAsync();
+
+                // Get HTML
+                var htmlstring = await result.Content.ReadAsStringAsync()
+                var html = new HtmlDocument();
+                html.LoadHtml(htmlstring);
+
+                // parse the XML value,  div class = "TableObject-item TableObject-item--primary" 
+                // got using inspection on the website
+                var items = html.DocumentNode.Descendants("div")
+                            .Where(div => div.GetAttributeValue("class", "")
+                            .Equals("TableObject-item TableObject-item--primary")).ToList();
+
+                var eventList = new List<EventModel>();
+
+                // create the list of events and time for JSON
+                foreach (var item in items)
+                {
+                    var eventModel = new EventModel()
+                    {
+                        eventType = item.Descendants("span")
+                            .Where(span => span.GetAttributeValue("class", "").Equals("audit-type")).FirstOrDefault().InnerText,
+                        time = Convert.ToDateTime(item.Descendants("relative-time").FirstOrDefault().ChildAttributes("datetime").FirstOrDefault().Value)
+                    };
+
+                    eventList.Add(eventModel);
+                }
+              
+                // Convert to JSON and return for display
+                jsonResult = JsonConvert.SerializeObject(eventList);
 
             }
-            else
+           else
             {
-                response = "Failed to connect to GitHub";
+                jsonResult = result.ReasonPhrase;
             }
 
 
-            return response;
-
+            return jsonResult;
 
         }
+
 
     }
 }
